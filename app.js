96be4153fdb6ad1b2786bc555cb44f514f8d744c
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-09-01 01:53:30 -0700

    Thrive style

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-var metronome = new Metronome();
-var tempo = document.getElementById('tempo');
-tempo.textContent = metronome.tempo;
+var metronome = new Metronome()
+var tempo = document.getElementById("tempo")
+tempo.textContent = metronome.tempo
 
-var playPauseIcon = document.getElementById('play-pause-icon');
+var playPauseIcon = document.getElementById("play-pause-icon")
 
-var playButton = document.getElementById('play-button');
-playButton.addEventListener('click', function() {
-    metronome.startStop();
+var playButton = document.getElementById("play-button")
+playButton.addEventListener("click", function () {
+  metronome.startStop()
 
-    if (metronome.isRunning) {
-        playPauseIcon.className = 'pause';
-    }
-    else {
-        playPauseIcon.className = 'play';
-    }
+  if (metronome.isRunning) {
+    playPauseIcon.className = "pause"
+  } else {
+    playPauseIcon.className = "play"
+  }
+})
 
-});
-
-var tempoChangeButtons = document.getElementsByClassName('tempo-change');
+var tempoChangeButtons = document.getElementsByClassName("tempo-change")
 for (var i = 0; i < tempoChangeButtons.length; i++) {
-    tempoChangeButtons[i].addEventListener('click', function() {
-        metronome.tempo += parseInt(this.dataset.change);
-        tempo.textContent = metronome.tempo;
-    });
+  tempoChangeButtons[i].addEventListener("click", function () {
+    metronome.tempo += parseInt(this.dataset.change)
+    tempo.textContent = metronome.tempo
+  })
 }
+
+var tempoInput = document.getElementById("tempo-input")
+var tempoUpdate = document.getElementById("tempo-input-button")
+tempoUpdate.addEventListener("click", function () {
+  metronome.tempo = parseInt(tempoInput.value)
+  tempo.textContent = metronome.tempo
+})
