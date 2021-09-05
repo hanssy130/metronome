114b39e48f2ee423e6de4c52b08d9749fd7830e1
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-09-05 06:54:32 -0700

    add express server and node.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express")
 const path = require("path")
 const app = express()
 app.use(express.json())
-app.use(express.static("express"))
+app.use(express.static("public"))
+
 // default URL for website
 app.use("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"))
